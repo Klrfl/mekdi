8ac3f5fd26cb1e143a7dc25de7b8dd6dc38e7050
Efraim Munthe <efrayanglain@gmail.com>
2024-02-22 11:45:50 +0700

    render view

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi i love %s", r.URL.Path[1:])
+	filepath := path.Join("views", "index.html")
+	tmpl, err := template.ParseFiles(filepath)
+
+	if err != nil {
+		http.Error(w, "anjing error cuk", http.StatusInternalServerError)
+	}
+
+	data := map[string]interface{}{
+		"name": "siapa kek",
+		"data": "blom ada",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
