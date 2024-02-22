2e680061d6f3ce4b24064fcd6027cbf709d50ce3
Efraim Munthe <efrayanglain@gmail.com>
2024-02-22 11:12:05 +0700

    make one route

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi i love %s", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("anjay")
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
