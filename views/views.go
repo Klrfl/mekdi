382b3a3531e2be383fffee34c3e50380632882cc
Efraim Munthe <efrayanglain@gmail.com>
2024-02-24 20:11:06 +0700

    add resets.css file and prettify the frontend in general

--- views/views.go
+++ views/views.go
@@ -8,5 +8,6 @@ import (
 func RenderPage(file string) *template.Template {
 	layout := path.Join("views", "layouts", "baseLayout.html")
 	navbar := path.Join("views", "components", "navbar.html")
-	return template.Must(template.ParseFiles(layout, navbar, file))
+	footer := path.Join("views", "components", "footer.html")
+	return template.Must(template.ParseFiles(layout, navbar, footer, file))
 }
